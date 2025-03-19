fa2a5f4eea72c66c1f68d763c305045fed25b992
joshuaconstine <joshua.constine97@gmail.com>
2025-03-19 16:54:29 -0700

    working frontend, added theme colors

---
+++ frontend/travel-buddy/src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <nav className="bg-branded-600">
+        <div className="flex w-full justify-between items-center">
+              <div className="flex bg-branded-600 items-center gap-4 branded-600">
+                <h1>Travel Buddy!</h1>
+                <span>your simple travel assistant</span>
+              </div>
+          </div> 
+      </nav>
+      <main className="flex flex-col gap-[32px] row-start-  items-center sm:items-start">
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
