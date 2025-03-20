b4632c0ec2f1d55c854e4cba30703c86b9e718bc
irmtou <elee200@hotmail.com>
2025-03-19 18:00:01 -0700

    Update trip_item.py
    I referred to the backend design doc, but I'm a bit unsure on if we decided if the cost of the trip item was required or optional. I may have heard that it was optional during one of our meetings, but it shows required in the docs.

---
+++ backend/src/travel_buddy/models/trip_item.py
@@ -0,0 +1,19 @@
+from datetime import UTC, datetime
+
+from travel_buddy.core.database import Base
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer, String, Enum
+from sqlalchemy.orm import relationship
+
+class Item(Base):
+    __tablename__ = "items"
+
+    item_id = Column(Integer, primary_key=True, index=True, nullable=False, autoincrement=True)
+    trip_id = Column(Integer, ForeignKey("trips.trip_id"))
+    title = Column(String, nullable=False) # false = Required
+    date = Column(DateTime, nullable=False, default=datetime.now(UTC))
+    item_type = Column(Enum("accommodation", "transportation", "activity", "other", name = "item_types"), nullable=False)
+    description = Column(String, nullable=True)
+    cost = Column(Float, nullable = False)
+    web_link = Column(String, nullable = True)
+
+    trip = relationship("Trip", back_populates="items") # relationship to Trip (class) model
