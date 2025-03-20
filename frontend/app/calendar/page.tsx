8e87a823a00d498fe9b114bd1775db92ddaa1b3b
joshuaconstine <joshua.constine97@gmail.com>
2025-03-20 15:41:54 -0700

    working frontend in typescripe

---
+++ frontend/app/calendar/page.tsx
@@ -0,0 +1,14 @@
+import {Navbar} from "@/components/navbar";
+
+export default function Home() {
+  return (
+      <>
+      <Navbar/>
+      <main className="flex flex-col gap-[32px] row-start-  items-center sm:items-start">
+        Calendar view
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </footer>
+      </>
+  );
+}
