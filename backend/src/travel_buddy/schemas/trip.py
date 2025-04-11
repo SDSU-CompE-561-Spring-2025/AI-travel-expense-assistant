4af0d6742a45feeb4b6b152819679325e7fb262a
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-04-11 13:29:48 -0700

    simplifying trip model
    deleted unnecessary functions and fixed up TripCreate (added user_id, removed config)

--- backend/src/travel_buddy/schemas/trip.py
+++ backend/src/travel_buddy/schemas/trip.py
@@ -1,26 +1,13 @@
 from datetime import datetime
 from pydantic import BaseModel, constr, Field
-
-
-# validate trip inputs
-class TripBase(BaseModel):
-    title: constr(min_length=3, max_length=50, pattern=r"^[a-zA-Z0-9_]+$")
-    start_date: datetime.date
-    end_date: datetime.date
-
-class TripCreate(TripBase):
-    description: constr(min_length=0, max_length=200, pattern=r"^[a-zA-Z0-9_]+$")
-
     
-class Trip(TripBase):
-    id: int = Field(..., gt=0)
+
+class TripCreate(BaseModel):
     title: constr(min_length=3, max_length=50, pattern=r"^[a-zA-Z0-9_]+$")
     start_date: datetime.date
     end_date: datetime.date
-    description: constr(min_length=0, max_length=200, pattern=r"^[a-zA-Z0-9_]+$")
-
-    class Config:
-        from_attributes = True
+    description: constr(min_length=0, max_length=200, pattern=r"^[a-zA-Z0-9_]+$") | None
+    user_id: int = Field(..., gt=0)
 
 
 class TripResponse(BaseModel):
@@ -28,7 +15,7 @@ class TripResponse(BaseModel):
     title: constr(min_length=3, max_length=50, pattern=r"^[a-zA-Z0-9_]+$")
     start_date: datetime.date
     end_date: datetime.date
-    description: constr(min_length=0, max_length=200, pattern=r"^[a-zA-Z0-9_]+$")
+    description: constr(min_length=0, max_length=200, pattern=r"^[a-zA-Z0-9_]+$") | None
 
     class Config:
         from_attributes = True
