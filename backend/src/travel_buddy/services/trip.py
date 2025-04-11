2be8c0001048f66a9ed954a823926adb33c6e7c9
sjpitman22 <sjpitman22@gmail.com>
2025-04-11 13:29:49 -0700

    added remaining CrUD operations to trip.py
    added create_trip() and delete_trip(), will edit as needed to work w/ existing models/routes

--- backend/src/travel_buddy/services/trip.py
+++ backend/src/travel_buddy/services/trip.py
@@ -1,5 +1,4 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import Integer
 from typing import Optional
 from fastapi import HTTPException, status
 from travel_buddy.core.config import get_settings
@@ -11,14 +10,28 @@ settings = get_settings()
 
 #CrUD Operations
 
-def create_trip(db: Session, trip: TripCreate):
-    return
+def create_trip(db: Session, trip: TripCreate, user: User):
+    db_trip = Trip(
+        user_id = user.id,
+        title = trip.title,
+        start_date = trip.start_date,
+        end_date = trip.end_date,
+        description = trip.description
+    )
+    return db_trip
 
 def get_trips(db: Session, user: User):
     return db.query(Trip).filter(Trip.user_id == user.id).all
 
-def get_trip_by_id(db: Session, id: Integer):
+def get_trip_by_id(db: Session, id: int):
     return db.query(Trip).filter(Trip.id == id).first()
 
-def delete_trip(db: Session, id: Integer):
-    return
+def delete_trip(db: Session, id: int):
+    trip = get_trip_by_id(Session, id)
+    if not trip:
+        return None
+    title = trip.title
+    db.delete(trip)
+    db.commit()
+
+    return {"message": f"Trip {title} with ID {id} has been successfully removed"}
