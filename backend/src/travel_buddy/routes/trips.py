6be4bbd27227dfab5bc501e04db12a894d612c48
sjpitman22 <sjpitman22@gmail.com>
2025-04-13 15:28:17 -0700

    finish trip routes
    create router endpoints for trip services

--- backend/src/travel_buddy/routes/trips.py
+++ backend/src/travel_buddy/routes/trips.py
@@ -4,25 +4,43 @@ from travel_buddy.schemas.trip import TripCreate, TripResponse
 from travel_buddy.dependencies import get_db
 from fastapi import Depends
 from sqlalchemy.orm import Session
-#from travel_buddy.core.auth import oauth2_scheme, decode_access_token
-#import travel_buddy.services.trip as trip_service ## create this in services??
-#import travel_buddy.services.user as user_service
+from travel_buddy.core.authentication import oauth2_scheme, decode_access_token
+import travel_buddy.services.trip as trip_service
+import travel_buddy.services.user as user_service
 
 router = APIRouter()
 
-'''
 @router.post("/", response_model=TripResponse)
 def create_trip(
     trip: TripCreate,
     db: Session = Depends(get_db),
-    #token: str = Depends(oauth2_scheme),
+    token: str = Depends(oauth2_scheme),
 ):
-    #username = decode_access_token(token).username
-    #user = user_service.get_user_by_username(db, username)
+    username = decode_access_token(token).username
+    user = user_service.get_user_by_username(db, username)
 
-    #user_id = user.id
+    return trip_service.create_trip(db, trip, user)
 
-    return trip_service.create_trip(db, trip, user_id)
+@router.get("/trips", response_model=TripResponse)
+def get_trips(
+    db: Session = Depends(get_db),
+    token: str = Depends(oauth2_scheme)
+):
+    username = decode_access_token(token).username
+    user = user_service.get_user_by_username(db,username)
 
-'''
+    return trip_service.get_trips(db,user)
 
+@router.get("/trips/{id}", response_model=TripResponse)
+def get_trip_by_id(
+    db: Session = Depends(get_db),
+    id: int=-1
+):
+    return trip_service.get_trip_by_id(db,id)
+
+@router.delete("/trips/{id}",response_model=TripResponse)
+def delete_trip(
+    db: Session = Depends(get_db),
+    id: int=-1
+):
+    return trip_service.delete_trip(db,id)
