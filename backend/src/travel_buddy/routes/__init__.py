af9627810766bc7752fbb9a9b59ccf052536378a
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-04-13 18:37:18 -0700

    update routes init & fixed issue with trip schema
    changed to proper datetime usage & fixed router inclusions

--- backend/src/travel_buddy/routes/__init__.py
+++ backend/src/travel_buddy/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from travel_buddy.routes import user
+from travel_buddy.routes import user, trips, trip_item
 
 api_router = APIRouter()
 
@@ -14,14 +14,16 @@ api_router.include_router(
 # api_router.include_router(category.router, prefix="/category", tags=["Category"])
 
 api_router.include_router(
-    user.router, 
+    trips.router, 
     prefix="/trip", 
     tags=["Trip"]
     )
 
+'''
 api_router.include_router(
-    user.router, 
+    trip_item.router, 
     prefix="/item", 
     tags=["Item"]
     )
+'''
 
