7e525c68f0c831981bd416ada99fadfd5d7789e6
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-04-17 17:34:56 -0700

    recommendations component creation
    created /components/recommendations.tsx and added basic functionality, then imported/accessed it in page.tsx

--- frontend/app/recommendations/page.tsx
+++ frontend/app/recommendations/page.tsx
@@ -1,12 +1,16 @@
 import {Navbar} from "@/components/navbar";
+import {Recommendations} from "@/components/recommendations"
+
 
 export default function Home() {
   return (
       <>
       <Navbar/>
       <main className="flex flex-col gap-[32px] row-start-  items-center sm:items-start">
-        Reccomendations
+        Recommendations
       </main>
+
+      <Recommendations />
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
       </>
---
+++ frontend/components/recommendations.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+
+const Recommendations = () => {
+    return (
+        <>
+            <div className='max-w-5xl mx-auto'>
+                <h1 className='text-3xl font-bold mb-2'>Travel Recommendations</h1>
+
+            </div>
+        
+        
+        
+        
+        </>
+    )
+}
+export {Recommendations}
