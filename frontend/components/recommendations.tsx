1bb171e07d5ffddc1ab1ed13b6f2ec2bfbb198b4
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-04-17 17:42:26 -0700

    added ui card, input, and separator
    using npx shadcn@latest add ___ I added the /components/ui/card, input, and separator for the recommendations page

--- frontend/components/recommendations.tsx
+++ frontend/components/recommendations.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { useState } from "react"
+//import { useChat } from "ai/react"
+import { Send } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+//import RecommendationCard from "./recommendation-card"
 
 const Recommendations = () => {
     return (
         <>
             <div className='max-w-5xl mx-auto'>
                 <h1 className='text-3xl font-bold mb-2'>Travel Recommendations</h1>
-
+                <p className="text-gray-500 mb-8">Discover the best places to visit at your next destination</p>
             </div>
         
         
