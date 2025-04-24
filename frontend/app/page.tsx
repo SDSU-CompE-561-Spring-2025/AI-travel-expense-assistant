18d47aa57c787a907881bf4c811c0726674a2bc0
joshuaconstine <joshua.constine97@gmail.com>
2025-04-23 18:22:46 -0700

    working showing a users trips on the home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,23 @@
+'use client';
 import Navbar from "@/components/navbar";
+import { useUserTrips } from "@/hooks/useTrips";
 
 export default function Home() {
+ const {trips, loading, error, refetch} = useUserTrips();
+    console.log(trips);
   return (
       <>
       <Navbar/>
       <main className="flex flex-col gap-8 items-center sm:items-start">
-        Home
+        {trips.map((trip) => (
+          <div key={trip.id}>
+            <h1>{trip.title}</h1>
+            <p>{trip.description}</p>
+            <p>{trip.start_date}</p>
+            <p>{trip.end_date}</p>
+           
+          </div>
+        ))}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
