036751dbaa2793dfd36bae56b92d903d168d400e
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-04-28 09:09:50 -0700

    Fixed incomplete html class
    in <main> finished row-start- to be row-start-3

--- frontend/app/calendar/page.tsx
+++ frontend/app/calendar/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <>
       <Navbar/>
-      <main className="flex flex-col gap-[32px] row-start-  items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-3 items-center sm:items-start">
         Calendar view
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
