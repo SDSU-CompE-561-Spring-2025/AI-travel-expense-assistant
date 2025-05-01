b7be2503f2289c5ced01407f6ad66f64c38b16fa
BriannavLy <Briannavly@gmail.com>
2025-04-30 17:36:58 -0700

    Update ForgotPasswordForm.tsx
    - Changed top left button to home page instead of login page

--- frontend/components/ForgotPasswordForm.tsx
+++ frontend/components/ForgotPasswordForm.tsx
@@ -16,8 +16,8 @@ export function ForgotPasswordForm() {
   return (
     <main className="flex flex-col min-h-screen w-full items-center bg-gradient-to-b from-branded-900 to-white p-4">
       <div className="w-full flex p-4">
-        <Link href="/login">
-          <Button variant="outline">Back to login</Button>
+        <Link href="/">
+          <Button variant="outline">Back to home</Button>
         </Link>
       </div>
 
