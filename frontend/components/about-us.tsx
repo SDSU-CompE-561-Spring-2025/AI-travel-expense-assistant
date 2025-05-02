831a13a6af458286a0f782fb51f4cea84765f25a
Samantha James <126624025+SamJames13@users.noreply.github.com>
2025-05-02 15:49:55 -0700

    minor about us updates
    added card borders and changed who are we statement

--- frontend/components/about-us.tsx
+++ frontend/components/about-us.tsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
             <p className="text-lg  max-w-2xl mx-auto">
               The engineers here are Travel Buddy want to help you make your travel experience as smooth as possible. Our goal 
               is to provide a secure, all-in-one travel itinerary planner platform where you can create, edit, and manage your 
-              trips easily. We began as teammates in SDSU's COMPE 561 and are now all super successful in our technology careers 
+              trips easily. We began as teammates in SDSU's COMPE 561 and now we are all super successful in our technology careers 
               (or at least manifestating this). Thank you for your interest in our application!
             </p> <br /><br />
 
@@ -17,7 +17,7 @@ export default function AboutPage() {
             <br />
 
             <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 mb-16">
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Sam James
@@ -44,7 +44,7 @@ export default function AboutPage() {
               </CardContent>
               </Card>
 
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Erica Lee
@@ -71,7 +71,7 @@ export default function AboutPage() {
               </CardContent>
               </Card>
 
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Brianna Ly
@@ -100,7 +100,7 @@ export default function AboutPage() {
             </div>
 
             <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 mb-16">
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Joshua Constine
@@ -127,7 +127,7 @@ export default function AboutPage() {
               </CardContent>
               </Card>
 
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Theo Bongolan
@@ -154,7 +154,7 @@ export default function AboutPage() {
               </CardContent>
               </Card>
 
-              <Card className="bg-[#C6AFEE]">
+              <Card className="bg-[#C6AFEE] border-[#A279EA] border-3">
               <CardHeader className="pb-2">
                 <CardTitle className="text-xl flex items-center gap-2">
                   Sean Pitman
